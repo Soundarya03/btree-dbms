b0398329aae37fc0dc99778d933246fa6bf9672a
Soundarya03 <soundarya1103@gmail.com>
2020-11-22 22:19:18 +0530

    shubh aarambh xD

---
+++ btree.c
@@ -0,0 +1,12 @@
+//this file can contain the data structure definition
+typedef struct btreeNode *btreePointer;
+
+typedef struct btreeNode
+{
+    int *key;               //array of keys
+    btreePointer *children; //array of pointers to children;
+
+    int t;    //minimum degree(no. of keys that should be there)
+    int n;    //current no. of keys in this node
+    int leaf; //0 OR 1 : whether the node is a leaf or not
+} btreeNode;
---
+++ main.c
@@ -0,0 +1,4 @@
+/*
+    DS project: 20 mark component
+    Topic: Basic DBMS implementation using B-trees
+*/
