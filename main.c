68721f3356a6edf33fef3c04dad242a44546182c
swetha-h <swethaswethu0002@gmail.com>
2020-11-23 22:50:39 +0530

    checking out git

--- main.c
+++ main.c
@@ -2,3 +2,20 @@
     DS project: 20 mark component
     Topic: Basic DBMS implementation using B-trees
 */
+#include <stdio.h>
+#include <string.h>
+int empty(string input)
+{
+    if(input) //condition to check if empty.  
+}
+int main()
+{
+    while(1)
+    {
+        //input the string check how
+
+        if(empty(input)) continue;
+
+    }
+    return 0;
+}
