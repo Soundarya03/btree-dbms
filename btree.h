3e2d55ff45f85c9b0ba5266c1983170f976bb0ad
Soundarya03 <soundarya1103@gmail.com>
2020-12-14 17:25:40 +0530

    restructuring btree

--- btree.h
+++ btree.h
@@ -1,17 +1,17 @@
 //this file can contain the data structure definition
 #include <stdio.h>
 #include <string.h>
-
+#define M 3
 //typedef struct btreeNode *btreePointer;
 
 typedef struct _node
 {
-    int *keys;              //array of keys
-    struct _node *children; //array of nodes children;
+    int keys[M - 1];           //array of keys
+    struct _node *children[M]; //array of POINTERS children;
 
-    int t;    //minimum degree(no. of keys that should be there)
-    int n;    //current no. of keys in this node
-    int leaf; //0 OR 1 : whether the node is a leaf or not
+    //const int t;    //minimum degree(no. of keys that should be there)
+    int n;          //current no. of keys in this node
+    short int leaf; //0 OR 1 : whether the node is a leaf or not
 
 } btreeNode;
 
